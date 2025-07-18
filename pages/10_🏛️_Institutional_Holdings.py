b48649e0c6f2db6cf96838667c5d39a5ca7c5d29
Risky46 <sohamasaha@gmail.com>
2025-07-18 23:57:14 +0530

    Update 10_🏛️_Institutional_Holdings.py

--- pages/10_🏛️_Institutional_Holdings.py
+++ pages/10_🏛️_Institutional_Holdings.py
@@ -2,6 +2,8 @@ import streamlit as st
 import requests
 import pandas as pd
 from datetime import datetime
+import streamlit as st
+import base64
 def get_base64_image(image_path):
     with open(image_path, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode()
